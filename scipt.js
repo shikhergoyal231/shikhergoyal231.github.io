44d10ce4484456d0421ad7ba368ac7801c882bcc
shikhergoyal231 <68539542+shikhergoyal231@users.noreply.github.com>
2020-09-30 00:37:54 +0530

    Update scipt.js

--- scipt.js
+++ scipt.js
@@ -1,16 +1,24 @@
-$(".email-signup").hide();
-$("#signup-box-link").click(function(){
-  $(".email-login").fadeOut(100);
-  $(".email-signup").delay(100).fadeIn(100);
-  $("#login-box-link").removeClass("active");
-  $("#signup-box-link").addClass("active");
-});
-$("#login-box-link").click(function(){
-  $(".email-login").delay(100).fadeIn(100);;
-  $(".email-signup").fadeOut(100);
-  $("#login-box-link").addClass("active");
-  $("#signup-box-link").removeClass("active");
-});
+function validate(){
+var username = document.getElementById("firstname").value;
+var password = document.getElementById("lastname").value;
+if ( username == "Formget" && password == "formget#123"){
+alert ("Login successfully");
+window.location = "index.html"; // Redirecting to other page.
+return false;
+}
+else{
+attempt --;// Decrementing by one.
+alert("You have left "+attempt+" attempt;");
+// Disabling fields after 3 attempts.
+if( attempt == 0){
+document.getElementById("firstname").disabled = true;
+document.getElementById("lastname").disabled = true;
+document.getElementById("Sign Up").disabled = true;
+return false;
+}
+}
+}
+
 
 $.ajax(
 {
