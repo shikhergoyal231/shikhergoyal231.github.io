6c53834ab706b56ab17c3b97558c9433feb0c825
shikhergoyal231 <68539542+shikhergoyal231@users.noreply.github.com>
2020-09-30 00:18:33 +0530

    Update scipt.js

--- scipt.js
+++ scipt.js
@@ -14,15 +14,13 @@ $("#login-box-link").click(function(){
 
 $.ajax({
     url: "https://reqres.in/api/users",
-    type: "POST",
-    data: {
-        name: "paul rudd",
-        movies: ["I Love You Man", "Role Models"]
-    },
-    success: function(response){
-        console.log(response);
-    }
+    data: {},
+    beforeSend: function(){}
+}).done(function(response) {
+    var trArr = new Array();
+    $.each(response.data, function(i, v){
+    	trArr.push('<tr><td>' + v.id + '</td><td>' + v.first_name + '</td><td>' + v.last_name + '</td><td><img src="' + v.avatar + '" width="120px" /></td></tr>');
+    });
+    $('table#alk-table tbody').append(trArr.join('\n'));
 });
-	
-
 
